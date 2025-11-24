57d7d989af8531ee59566c70451bef964af546ce
ok <ok@shaman.land>
2025-11-25 00:54:39 +0200

    Rename module 'app' to 'lib'; update dependencies

--- lib/src/main/java/org/graphstream/ui/android_viewer/util/DefaultFragment.java
+++ lib/src/main/java/org/graphstream/ui/android_viewer/util/DefaultFragment.java
@@ -2,12 +2,13 @@ package org.graphstream.ui.android_viewer.util;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.ui.android.AndroidFullGraphRenderer;
